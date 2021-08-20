553ce77d327df753522c1a627729dbde818f49a9
MarauderPixie <tobias.anton.85@gmail.com>
2021-08-20 12:15:43 +0200

    first commit

---
+++ 04_trials.js
@@ -0,0 +1,59 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    forced_choice: [
+        // CATEGORY A
+        {
+            question: "",
+            picture: "images/c-l-b.png",
+            option1: 'A',
+            option2: 'B',
+            correct: 'A',
+        },{
+            question: "",
+            picture: "images/c-s-b.png",
+            option1: 'A',
+            option2: 'B',
+            correct: 'A',
+        },{
+            question: "",
+            picture: "images/c-l-w.png",
+            option1: 'A',
+            option2: 'B',
+            correct: 'A',
+        },{
+            question: "",
+            picture: "images/c-s-w.png",
+            option1: 'A',
+            option2: 'B',
+            correct: 'A',
+        },
+        // CATEGORY B
+        {
+            question: "",
+            picture: "images/s-l-b.png",
+            option1: "A",
+            option2: "B",
+            correct: "B"
+        },{
+            question: "",
+            picture: "images/s-l-w.png",
+            option1: "A",
+            option2: "B",
+            correct: "B"
+        },{
+            question: "",
+            picture: "images/s-s-b.png",
+            option1: "A",
+            option2: "B",
+            correct: "B"
+        },{
+            question: "",
+            picture: "images/s-s-w.png",
+            option1: "A",
+            option2: "B",
+            correct: "B"
+        }
+    ]
+};
